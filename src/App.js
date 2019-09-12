b6d8bcfefb3944052b8cbb6b9562c70a6db8c80a
Trojan-Cat <kajarrod@gmail.com>
2019-09-12 16:17:30 +1000

    added in geolocation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Home from "./components/Home";
 import Map from "./components/Map";
 
 function App() {
+  const [markers, setMarker] = useState([]);
+  // const [startLoc, setStartLoc] = useState({ lat: -34.397, lng: 150.644 });
+
+  // Check if we can get the geolocation
+  if (navigator.geolocation) {
+    // 🗺️ yep, we can proceed!
+    navigator.geolocation.getCurrentPosition(displayLocationInfo);
+  } else {
+  }
+
+  //Send question to user to ask if we can use their location for a starting location
+  function displayLocationInfo(position) {
+    const lng = position.coords.longitude;
+    const lat = position.coords.latitude;
+    console.log("RAN");
+
+    console.log(`longitude: ${lng} | latitude: ${lat}`);
+    //TODO: This loops and rerenders the component each time it is set, and sets it back to the start etc
+    //setStartLoc({ startLoc: { lat: lat, lng: lng } });
+  }
+
+  const handleClickedMap = e => {
+    let latitude = e.latLng.lat();
+    let longtitude = e.latLng.lng();
+
+    const marker = { latitude: latitude, longtitude: longtitude };
+    setMarker([...markers, marker]);
+  };
+
   return (
     <div className="App">
       <Home />
       <Map
+        handleClickedMap={handleClickedMap}
         isMarkerShown
         googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
         loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `50vh` }} />}
+        containerElement={<div style={{ height: `70vh` }} />}
         mapElement={<div style={{ height: `100%` }} />}
-        markers={[
-          { lat: -37.716871, lng: 144.8898329 },
-          { lat: -35.716871, lng: 144.8898329 }
-        ]}
+        // startLoc={startLoc}
+        markers={markers}
       />
     </div>
   );
 }
 
-//const markers = { lat: -34.397, lng: 150.644 };
-
 export default App;
+
+//defaultCenter = {{ lat: -34.397, lng: 150.644 }}
--- src/components/Map.js
+++ src/components/Map.js
@@ -8,14 +8,31 @@ import {
 
 const Map = withScriptjs(
   withGoogleMap(props => (
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-      {props.markers.lat}
+    <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{ lat: -34.397, lng: 150.644 }}
+      onClick={props.handleClickedMap}
+    >
       {props.isMarkerShown &&
         props.markers.map(marker => (
-          <Marker position={{ lat: marker.lat, lng: marker.lng }} />
+          <Marker
+            position={{
+              lat: marker.latitude,
+              lng: marker.longtitude
+            }}
+          />
         ))}
     </GoogleMap>
   ))
 );
 
 export default Map;
+
+//defaultCenter = {{ lat: -34.397, lng: 150.644 }}
+/*
+ defaultCenter={{
+        lat: props.startLoc.lat,
+        lng: props.startLoc.lng
+      }}
+
+      */
